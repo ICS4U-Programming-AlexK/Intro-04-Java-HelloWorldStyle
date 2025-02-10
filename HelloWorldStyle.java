013c4aab09cf50b0ab2509a82748841b7b2cdf6d
Alex Kapajika <alex.kapajika.ntumba@stu.ocsb.ca>
2025-02-10 15:50:56 +0000

    Add HelloWorldStyle.java

---
+++ HelloWorldStyle.java
@@ -0,0 +1,21 @@
+/**
+
+* The HelloWorld program implements an application that
+
+* simply displays "Hello World!" to the standard output.
+
+*
+
+* @author  Alex Kapajika
+
+* @version 1.0
+
+* @since   2025-02-10
+
+*/
+
+class HelloWorldStyle{
+    public static void main(String[] args) {
+        System.out.println("HelloWorld, Alex!");
+    }
+}
